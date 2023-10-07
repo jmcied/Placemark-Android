f385c4ce713114fd1cb54c06fb501761d94fd550
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-10-07 19:41:38 +0100

    MainApp

---
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -0,0 +1,16 @@
+package org.wit.placemark.main
+
+import android.app.Application
+import org.wit.placemark.models.PlacemarkModel
+import timber.log.Timber
+import timber.log.Timber.Forest.i
+
+class MainApp : Application() {
+
+    val placemarks = ArrayList<PlacemarkModel>()
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        i("Placemark started")
+    }
+}
